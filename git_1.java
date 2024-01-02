d992f9a3f6f70d7cbd75a58fd80d881d8dd41e6e
Arpit Gupta <arpitgupta4957@gmail.com>
2024-01-02 15:26:53 +0530

    feature signup working

---
+++ git_1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class git_1 {
+    public static ArrayList<ArrayList<Integer>> findMat(int []num) {
+        Map<Integer,Integer> freq = new HashMap<>();
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+
+        for(int i: num) {
+            freq.put(i, freq.getOrDefault(i, 0)+1);
+            int countFreq = freq.get(i);
+
+            if(countFreq> ans.size()) {
+                ans.add(new ArrayList<>());
+            }
+            ans.get(countFreq -1).add(i);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int [] nums = {1,3,4,1,2,3,1};
+        System.out.println(findMat(nums));
+    }
+}
