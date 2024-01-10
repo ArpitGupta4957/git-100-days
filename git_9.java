3966468640a541834db33d174f7f0fd7946afb10
Arpit Gupta <arpitgupta4957@gmail.com>
2024-01-10 11:37:35 +0530

    today's code

---
+++ git_9.java
@@ -0,0 +1,25 @@
+public class git_9 {
+    static class Node{
+        int data;
+        Node left, right;
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static int height(Node root){
+        if(root == null){
+            return 0;
+        }
+        int left = height(root.left);
+        int right = height(root.right);
+        return Math.max(left, right) + 1;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        System.out.println(height(root));
+    }
+}
