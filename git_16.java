3217303db71c73f605dd00fa859aa3aa282e2a11
Arpit Gupta <arpitgupta4957@gmail.com>
2024-01-18 17:28:02 +0530

    today's code

---
+++ git_16.java
@@ -0,0 +1,22 @@
+public class git_16 {
+    public static int climbStairs(int n) {
+        if (n <= 1) {
+            return n;
+        }
+    
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+    
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+    
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(climbStairs(n));
+    }
+}
