7c0e53dc00961c94d54561b594fc82f8e41a77bb
Arpit Gupta <arpitgupta4957@gmail.com>
2024-03-25 19:52:17 +0530

    today's code

---
+++ git_82.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class git_82 {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<Integer>();
+        int[]count = new int[nums.length+1];
+        for(int freq : nums) 
+            count[freq]++;
+
+        for( int i =1 ; i <= nums.length ; i++){
+            if(count[i] == 2){
+                res.add(i);
+            }
+        }
+        return res;  
+    }
+    public static void main(String[] args) {
+        int [] arr = {4,3,2,7,8,2,3,1};
+        List<Integer> result = new git_82().findDuplicates(arr);
+        System.out.println(result);
+    }
+}
